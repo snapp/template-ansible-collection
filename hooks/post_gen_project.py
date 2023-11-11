de683f86d3bd86b53e7649d311c179fa76b7de7b
Christopher A. Snapp <snapp@redhat.com>
2023-11-11 15:02:25 -0700

    Modify antsibull changelog support to be optional
    Resolves: #19
    
    Signed-off-by: Christopher A. Snapp <snapp@redhat.com>

---
+++ hooks/post_gen_project.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import shutil
+
+REMOVE_PATHS = [
+    {% if not cookiecutter.enable_antsibull_changelog %}
+        '{{cookiecutter.collection_name}}/changelogs',
+        '{{cookiecutter.collection_name}}/CHANGELOG.rst',
+    {% endif %}
+]
+
+for path in REMOVE_PATHS:
+    path = path.strip()
+    try:
+        if path and os.path.exists(path):
+            os.unlink(path) if os.path.isfile(path) else shutil.rmtree(path)
+        else:
+            print(f"Path not found: {path}")
+    except Exception as e:
+        print(f"Error deleting {path}: {e}")
